07267f808f7fa0b7f00a2941c09725ee552dc66a
Sabir Aliyev <s.aliyev2005@gmail.com>
2025-10-31 10:45:41 +0400

    react-1

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,72 @@
+
+/*
+import Button from './Button.jsx'
+
+function App() {
+
+  return (
+    <Button/>
+  );
+
+ 
+}
+export default App
+
+-----------------------
+*/
+
+
+// props
+
+// props = read-only propersties that are shared between components.
+//      A parent component can send to a child component.
+//     < Component key=value />
+
+
+/*
+import Student from './4-Student.jsx'
+
+function App() {
+  return (
+    <>
+      <Student name="Spongebob" age={30} isStudent={true} />
+      <Student name="Patrick" age={30} isStudent={false} />
+      <Student name="Squidward" age={50} isStudent={false} />
+      <Student name="Sandy" age={27} isStudent={true} />
+      <Student name= "Larry" />
+    </>
+  );
+
+
+}
+
+-----------------------------------
+
+ 
+// UserGreeding  - If mentiqinin reacte kecmis hali
+
+ 
+import UserGreeting from './UserGreeting.jsx'
+
+function App() {
+
+  return (
+    <>
+
+      <UserGreeting isLoggedIn={true} username="Sabir" />
+    
+    </>
+  );
+
+}
+
+export default App
+
+*/
+
+
+function App(){
+
+}
+
+export default App
