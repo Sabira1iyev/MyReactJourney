d113b70f7bb145546512fc7e56aa1fb26d7ca2a3
Sabir Aliyev <s.aliyev2005@gmail.com>
2025-11-09 23:44:09 +0400

    new commit about lists

--- my-react-app/src/List.jsx
+++ my-react-app/src/List.jsx
@@ -1,10 +1,27 @@
-function List(){
+function List(props) {
 
-    const fruits = ["apple", "orange", "banana", "coconut", "pineapple"];
+    const itemlist = props.items;
+    
+    // const fruits = [{ id: 1, name: "apple", calories: 95 },
+    // { id: 2, name: "orange", calories: 45 },
+    // { id: 3, name: "banana", calories: 105 },
+    // { id: 4, name: "coconut", calories: 159 },
+    // { id: 5, name: "pineapple", calories: 37 }];
 
-    fruits.sort();
-    const listItems = fruits.map(fruit => <li>{fruit}</li>)
-    return(<ol>{listItems}</ol>)   // ul noqte ile siralayir, ol reqemlerle
+    // fruits.sort((a, b) => a.name.localeCompare(b.name)); // localcompare does letter sorting
+    //   fruits.sort((a, b) => b.name.localeCompare(a.name)); // reverse
+    //  fruits.sort((a, b) => a.calories - b.calories); // Numeric order
+    //    fruits.sort((a, b) => b.calories - a.calories); // reverse Numeric order
+
+    // const lowCalFruits = fruits.filter(fruit => fruit.calories < 100);
+
+   // const HighCalFruits = fruits.filter(fruit => fruit.calories >= 100);
+
+    const listItems = itemlist.map(item => <li key={item.id}>
+        {item.name}:&nbsp;
+        <b>{item.calories}</b></li>);
+
+    return (<ol>{listItems}</ol>)   // ul noqte ile siralayir, ol reqemlerle
 }
 
 export default List
