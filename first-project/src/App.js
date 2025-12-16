21ad651ab7c03fb61aefbeb9711cc9ca0c3c9086
Sabira1iyev <s.aliyev2005@gmail.com>
2025-12-16 06:50:07 -0500

    jsx

---
+++ first-project/src/App.js
@@ -0,0 +1,37 @@
+import './App.css';
+
+function App() {
+
+// const students = 43000;
+// const nameSurname = "Sabir Aliyev";
+// const dogrumu = true;
+// const date = 'date';
+// const url = "https://picsum.photos/250/250";
+
+  return (
+/* <div>
+<h1>Isim Soyisim: {nameSurname}</h1>
+<p>{students}</p>
+<p>dogrumu: {dogrumu}</p>
+
+
+{dogrumu ? <p>ogrenci sayisi</p> : <p>Kurs sayisi</p>} 
+
+<input type = {date}/>
+<img src={url} alt = ""/>
+</div> */
+
+/* <div className='box'>
+
+</div> */
+
+<div style={{
+  width:'250px',
+  height:'250px',
+  backgroundColor: "blue"
+
+}}></div>
+  );
+}
+
+export default App;
