901620d243fe3d944b351b0f0855a2e0324cb0ca
Sabir Aliyev <s.aliyev2005@gmail.com>
2026-02-06 01:15:45 +0400

    taskCreate

--- React-App/src/Components/TaskCreate.jsx
+++ React-App/src/Components/TaskCreate.jsx
@@ -1,13 +1,27 @@
+import { useState } from "react";
+
 function TaskCreate() {
+
+    const [title, setTitle] = useState('');
+    const [taskDesc, setTaskDesc] = useState('');
+
+    const handleChange = (event) => {
+        setTitle(event.target.value);
+    }
+
+    const handleTaskChange = (event) => {
+        setTaskDesc(event.target.value);
+    }
+
     return (
         <div className="task-create">
 
             <h3>Lutfen task ekleyiniz</h3>
             <form className="task-form">
                 <label className="task-label">Baslik</label>
-                <input className="task-input" />
+                <input value={title} onChange={handleChange} className="task-input" />
                 <label className="task-label">Task girin</label>
-                <textarea className="task-input" rows={5} />
+                <textarea value={taskDesc} onChange={handleTaskChange} className="task-input" rows={5} />
                 <button className="task-button">Olustur</button>
             </form>
         </div>
