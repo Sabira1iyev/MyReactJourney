f92ee6a4877a24a29035576f1262386cd007fb99
Sabir Aliyev <s.aliyev2005@gmail.com>
2026-02-17 00:30:27 +0400

    new project from youtube

---
+++ Youtube-Project/src/App.tsx
@@ -0,0 +1,17 @@
+import Message from './Message'
+
+// import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Message />
+    </>
+  )
+}
+
+export default App
---
+++ Youtube-Project/src/Message.tsx
@@ -0,0 +1,19 @@
+function Message() {
+
+    let name = "Sfaesvgbae";
+
+    function getName(){
+        if(name === "Sabir")return name;
+        else{
+            return "Aliyev";
+        }
+    }
+    return (
+        <>
+
+            <h1>Hello {getName()}</h1>
+        </>
+    )
+}
+
+export default Message;
