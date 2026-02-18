fe325f22ccead86ac1b0314bcdb1a6781e719fd7
Sabira1iyev <s.aliyev2005@gmail.com>
2026-02-18 09:04:12 -0500

    managing conctents

--- Youtube-Project/src/componenets/ListGroup.tsx
+++ Youtube-Project/src/componenets/ListGroup.tsx
@@ -1,14 +1,43 @@
 // import { Fragment } from 'react';
+// import type { MouseEvent } from "react";
+
+import { useState } from "react";
 
 function ListGroup() {
-  const items = ["New York", "San Fransisco", "Tokyo", "London", "Paris"];
+  let items = ["New York", "San Fransisco", "Tokyo", "London", "Paris"];
+
+  // const message = items.length === 0 ? <p>No item found</p> : null
+
+  // const getMessage = () => {
+  //   return;
+  // }
+
+  //Event handler
+  // const handleClick = (event: MouseEvent) => console.log(event);
+
+
+  const [selectedIndex, setselectedIndex] = useState(-1)
+
 
   return (
     <>
       <h1>List</h1>
+      {/* {items.length === 0 ? <p>No item found</p> : null} */}
+
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {setselectedIndex(index);}}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
