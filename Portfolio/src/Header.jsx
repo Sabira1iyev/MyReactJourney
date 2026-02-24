ece752d438a5196d960051aa1dcc39fcfceeefd2
Sabir Aliyev <s.aliyev2005@gmail.com>
2026-02-25 02:03:07 +0400

    porfolio

---
+++ Portfolio/src/Header.jsx
@@ -0,0 +1,20 @@
+function Header(){
+    return(
+        <>
+        <header className="header-container-mobile">
+            <div className="my-name">
+                <h3>Sabir Aliyev</h3>
+            </div>
+            <nav className="menu-section">
+                <a href="#">Home</a>
+                <a href="#">Skills</a>
+                <a href="#">About</a>
+                <a href="#">Active Projects</a>
+                <a href="#">Contact</a>
+            </nav>
+        </header>
+        </>
+    )
+}
+
+export default Header
