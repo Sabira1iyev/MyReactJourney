72fc42e37d94f84e089a04c54f4f6631bcfda221
Sabira1iyev <s.aliyev2005@gmail.com>
2026-02-26 10:25:10 -0500

    props

--- Tailwind-PROJECT/src/App.jsx
+++ Tailwind-PROJECT/src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 // const Title = () => {
 
@@ -21,35 +21,100 @@ import './App.css'
 //   )
 // }
 
+// const Mesage = (props) => {
+//   const { bgColor, text } = props;
 
-const Mesage = (props) => {
+//   return (
+//     <div
+//       style={{
+//         backgroundColor: bgColor,
+//         color: "blue",
+//       }}
+//     >
+//       {text}
+//     </div>
+//   );
+// };
 
-  const { color, text } = props;
+// function App() {
+//   const [count, setCount] = useState(0);
 
+//   return (
+//     <>
+//       <Mesage text="this is my first prop" bgColor="red" />
+//       <Mesage text="h1" bgColor="yellow" />
+//       <Mesage text="bye" bgColor="purple" />
+//     </>
+//   );
+// }
+
+// export default App;
+
+// const Box = (props) => {
+//   const { label, bgColor, textColor, large } = props;
+
+//   console.log(large);
+
+//   return (
+//     <div
+//       style={{
+//         backgroundColor: bgColor,
+//         color: textColor,
+//         fontSize: large ? '40px' : '12px',
+//         marginTop: '20px',
+//       }}
+//     >
+//       {label}
+//     </div>
+//   );
+// };
+
+// const App = () => {
+//   return (
+//     <div>
+//       <Box
+//         label="my name is Sabir"
+//         bgColor="CornflowerBlue"
+//         textColor="Green"
+//         large={true}
+//       />
+
+//       <Box
+//         label="Another one"
+//         bgColor="Lavender"
+//         textColor="Yellow"
+//         large = {false}
+//       />
+
+//       <Box
+//         label="green stuff"
+//         bgColor="Green"
+//         textColor="Yellow"
+//       />
+
+//     </div>
+//   );
+// };
+
+// export default App;
+
+const MyComponent = (props) => {
+  const { children } = props;
   return (
     <div>
- 
- 
-
+      <div>this is my component</div>
+      <div>{children}</div>
     </div>
-  )
-}
+  );
+};
 
-
-
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
+    <div>
+      <MyComponent>Hello Sabir</MyComponent>
+      <MyComponent>Goodbye</MyComponent>
+    </div>
+  );
+};
 
-      <Mesage text="this is my first prop"
-        color="red"
-
-      />
-
-    </>
-  )
-}
-
-export default App
+export default App;
