a2c3d19b0d83852cd44a327affd43a55ba4d919b
Sabir Aliyev <s.aliyev2005@gmail.com>
2026-02-27 01:30:56 +0400

    buttons

--- Portfolio/src/Header.jsx
+++ Portfolio/src/Header.jsx
@@ -1,18 +1,29 @@
-function Header(){
-    return(
+function Header() {
+
+
+const handleMenuClick = (element) =>{
+        element.preventDefault();
+        console.log("clicked");
+        
+}
+
+
+    return (
         <>
-        <header className="header-container-mobile">
-            <div className="my-name">
-                <h3>Sabir Aliyev</h3>
-            </div>
-            <nav className="menu-section">
-                <a href="#">Home</a>
-                <a href="#">Skills</a>
-                <a href="#">About</a>
-                <a href="#">Active Projects</a>
-                <a href="#">Contact</a>
-            </nav>
-        </header>
+            <header className="header-container-mobile">
+                <div className="header-content">
+                    <h3>SABIR ALIYEV</h3>
+                    <a href="#" className="menu-button" onClick={handleMenuClick}><i className="ri-menu-line"></i></a>
+                    <a href="#" className="close-button"><i className="ri-close-line"></i></a>
+                </div>
+                <nav className="menu-section">
+                    <a href="#">Home</a>
+                    <a href="#">Skills</a>
+                    <a href="#">About</a>
+                    <a href="#">Active Projects</a>
+                    <a href="#">Contact</a>
+                </nav>
+            </header>
         </>
     )
 }
