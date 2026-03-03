e3bc8b66c1904b4ef4df767bb9896ac685959649
Sabir Aliyev <s.aliyev2005@gmail.com>
2026-03-03 22:07:58 +0400

    home-container

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,18 +1,32 @@
 import Header from './Header'
 import Home from './Home'
 import { useState } from 'react'
- 
+
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const cppCode = `
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello world!"
+	string code = " Portfolio loaded... " ;
+	  for (char c : code) {
+		    cout << c << " ";
+	}
+	return 0;
+}
+  `
+
   return (
     <>
- <Header/>
- <Home/>
+      <Header />
+      <Home codeContent={cppCode} />
     </>
   )
 }
- 
+
 export default App 
--- Portfolio/src/Home.jsx
+++ Portfolio/src/Home.jsx
@@ -1,20 +1,36 @@
-import myImage from "./assets/image.png";
+import { useEffect, useState } from "react";
 
-function Home() {
+
+function Home({ codeContent }) {
+
+  const [text, setText] = useState("");
+useEffect(() => {
+  let index = 0;
+  const interval = setInterval(() => {
+    setText(codeContent.slice(0, index));
+    index++;
+    if (index > codeContent.length) clearInterval(interval);
+  }, 30);
+  return () => clearInterval(interval);
+}, [codeContent])
+
   return (
     <>
       <div className="home-container">
-        <div className="image-content">
-          <img src={myImage} alt="" />
+        <div className="code-content">
+          <pre>
+            <code>
+              {text}
+            </code>
+          </pre>
         </div>
         <div className="home-content">
           <h2 className="my-name">
             HI, <span>SABIR</span> IS HERE
           </h2>
           <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero
-            possimus animi laborum? Esse dolores excepturi labore animi sed
-            minima cupiditate.
+            Software Engineer focused on secure and scalable systems.
+            Passionate about clean code and continuous learning.
           </p>
         </div>
         <button
